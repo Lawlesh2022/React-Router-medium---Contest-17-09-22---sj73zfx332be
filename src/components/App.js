22a6b721ac7b8e221495a0327dafee4cfa897816
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-09-17 21:16:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,13 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 import { AppRoutes } from '../AppRoutes';
-import Index from './Pages/Index';
 
 import '../styles/App.css';
 const App = () => {
 
   return (
     <BrowserRouter>
-      <AppRoutes/>
-      <Routes>
-      <Route exact path = '/' element = {<Index/>} />
-      </Routes>
+      <AppRoutes/>      
     </BrowserRouter>
 
   )
